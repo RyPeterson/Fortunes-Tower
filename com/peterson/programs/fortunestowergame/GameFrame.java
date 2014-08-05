af5c4c2f3a06156b416a0d62fc2f9bd45bd082e0
sadjava <sadjava@users.noreply.github.com>
2014-08-04 19:42:42 -0700

    Update GameFrame.java
    Added functionality so that the "rules.txt" is read in independent of file system, aka relative to the generated class files.

--- com/peterson/programs/fortunestowergame/GameFrame.java
+++ com/peterson/programs/fortunestowergame/GameFrame.java
@@ -5,11 +5,18 @@ import com.peterson.programs.fortunestower.Deck;
 import com.peterson.programs.fortunestower.TestDeck;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Scanner;
 
 /**
@@ -213,7 +220,7 @@ public class GameFrame extends JFrame
      */
     private static class RulesLoader
     {
-        private static final String FILE = "./assets/rules.txt";
+        private static final String FILE = "rules.txt";
 
         /*
             Gets the rules of the game
@@ -223,16 +230,15 @@ public class GameFrame extends JFrame
             try
             {
                 StringBuilder b = new StringBuilder();
-                Scanner scan = new Scanner(new File(FILE));
-                while(scan.hasNextLine())
+                InputStream is = GameFrame.class.getResourceAsStream(FILE);
+                while(is.available() > 0)
                 {
-                    b.append(scan.nextLine());
-                    b.append("\n");
+                		b.append((char)is.read());
                 }
-                scan.close();
+                is.close();
                 return b.toString();
             }
-            catch (FileNotFoundException e)
+            catch (Exception e)
             {
                 /*
                     This should never happen; if it does, come beat me with a
